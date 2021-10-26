1dd0f447509ffedee70cfd47edbcb65c72d5c386
chemacenturion <centurion.philadelphia@gmail.com>
2021-10-25 22:57:19 -0400

    successful compiling w/ project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,19 +4,13 @@ import React from "react";
 
 function Project(props) {
     return (
-        <div className="card bg-dark text-white">
+        <div className="card">
             <img src={props.image} className="card-img" alt={props.name} />
             <div className="card-img-overlay">
                 <ul>
-                    <li>
-                        <strong>Name:</strong> {props.name}
-                    </li>
-                    <li>
-                        <strong>Project:</strong> {props.project}
-                    </li>
-                    <li>
-                        <strong>Demo:</strong> {props.location}
-                    </li>
+                    <li><strong>Name:</strong> {props.name}</li>
+                    <li><strong>Project:</strong> {props.project}</li>
+                    <li><a href={props.deployment} target="_blank" rel="noreferrer">Click Here For Deployed Application</a></li>
                 </ul>
             </div>
         </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -13,13 +13,13 @@ function Portfolio() {
             name={projects[0].name}
             image={projects[0].image}
             project={projects[0].project}
-            location={projects[0].location}
+            deployment={projects[0].deployment}
           />
           <Project
             name={projects[1].name}
             image={projects[1].image}
             project={projects[1].project}
-            location={projects[1].location}
+            deployment={projects[1].deployment}
           />
         </div>
         <div className="row">
@@ -27,13 +27,13 @@ function Portfolio() {
             name={projects[2].name}
             image={projects[2].image}
             project={projects[2].project}
-            location={projects[2].location}
+            deployment={projects[2].deployment}
           />
           <Project
             name={projects[3].name}
             image={projects[3].image}
             project={projects[3].project}
-            location={projects[3].location}
+            deployment={projects[3].deployment}
           />
         </div>
         <div className="row">
@@ -41,14 +41,14 @@ function Portfolio() {
             name={projects[4].name}
             image={projects[4].image}
             project={projects[4].project}
-            location={projects[4].location}
+            deployment={projects[4].deployment}
           />
 
           <Project
             name={projects[5].name}
             image={projects[5].image}
             project={projects[5].project}
-            location={projects[5].location}
+            deployment={projects[5].deployment}
           />
         </div>
       </div>
