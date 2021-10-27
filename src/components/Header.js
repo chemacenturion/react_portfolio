2411106b55a6d0e2ebe2595f23c2d35d8946fba3
chemacenturion <centurion.philadelphia@gmail.com>
2021-10-27 14:02:05 -0400

    added mobile navigation w/o functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { FaBars } from 'react-icons/fa'
 
 const Header = ({ currentPage, handlePageChange}) => {
     return (
         <>
-        <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+        <nav className="navigation">
+            <header className="header">José María Centurión</header>
+                <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+            <FaBars className="mobile-icon" />
+        </nav>
         </>
     )
 }
